c6ab6b938c152b63b36532679b6b2eaad6fd4bf4
sayekooie <mmsayekooie@gmail.com>
2021-03-06 13:09:36 +0330

    change it to spring-mvc project

---
+++ src/main/java/com/example/practice/config/RootConfig.java
@@ -0,0 +1,7 @@
+package com.example.practice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"com.example.practice.service"})
+public class RootConfig {
+}
